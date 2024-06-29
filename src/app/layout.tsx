208b3f2bc874081f38e41579610ef038604ddd9c
MrNyG25 <yesid19999@gmail.com>
2024-06-29 13:45:18 -0500

    refactor(main layout): move reactQueryProvider to general components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster"
-import { ReactQueryClientProvider } from "./components/ReactQueryProvider";
+import { ReactQueryClientProvider } from "@/components/ReactQueryProvider";
 
 
 const inter = Inter({ subsets: ["latin"] });
